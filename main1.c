03556d07d5d93ba1caa6a4fa85addade26ea05cc
AlissonBonatto <77208811+AlissonBonatto@users.noreply.github.com>
2022-08-12 14:24:24 -0300

    Update main1.c

--- main1.c
+++ main1.c
@@ -8,6 +8,32 @@
 
 int main(void)
 {
+	// Display como output
+	// Linhas como outputs e colunas como inputs
+	OUTPUT(D4);
+	OUTPUT(D5);
+	OUTPUT(D6);
+	OUTPUT(D7);
+	OUTPUT(E);
+	OUTPUT(RS);
+	
+	OUTPUT(LINHA1);
+	OUTPUT(LINHA2);
+	OUTPUT(LINHA3);
+	OUTPUT(LINHA4);
+	INPUT(COLUNA1);
+	INPUT(COLUNA2);
+	INPUT(COLUNA3);
+	INPUT(COLUNA4);
+	
+	
+	// Garante que todas as linhas comecem em nível lógico alto
+	HIGH(LINHA1);
+	HIGH(LINHA2);
+	HIGH(LINHA3);
+	HIGH(LINHA4);
+	
+	// Incialização do Display
 	functionSet();
 	entryModeSet(1, 0);
 	displayOnOffControl(1, 1, 1);
