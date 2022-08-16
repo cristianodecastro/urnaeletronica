f052c7575b3f95a20896ee040e46e6a545c72abc
AlissonBonatto <77208811+AlissonBonatto@users.noreply.github.com>
2022-08-16 01:44:21 -0300

    Add files via upload

---
+++ v4_16.08_projetoUrnaEletrônica/urna_eletronica/urna_eletronica/lcd.h
@@ -0,0 +1,21 @@
+#ifndef LCD_H_
+#define LCD_H_
+
+#define DOWN_ARROW_CHARACTER 0x00
+#define RIGHT_OPTION_CHARACTER 0x01
+#define LEFT_ARROW_CHARACTER 0x7F
+
+void functionSet(void);
+void entryModeSet(char, char);
+void displayOnOffControl(char, char, char);
+char getBit(char, char);
+void sendChar(char);
+void setDdRamAddress(char);
+void setCgRamAddress(char);
+void cursorHome();
+void sendString_setAdress(char*, char, char);
+void sendString(char*);
+void clearDisplay();
+void storage_special_characters();
+
+#endif // LCD_H_
